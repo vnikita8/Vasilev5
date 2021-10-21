660f6d9098955f302a6769df172f08b136d9252b
vnikita <vnikita123456@gmail.com>
2021-10-22 01:49:35 +0700

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace Vasilev5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Ex1 = new System.Windows.Forms.NumericUpDown();
+            this.Ex2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ex3 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Ex1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ex2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ex3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(290, 308);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(308, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(290, 308);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(604, 12);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(290, 308);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(364, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сгенерировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(660, 358);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 25);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Сгенерировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Ex1
+            // 
+            this.Ex1.Location = new System.Drawing.Point(169, 329);
+            this.Ex1.Name = "Ex1";
+            this.Ex1.Size = new System.Drawing.Size(53, 23);
+            this.Ex1.TabIndex = 7;
+            // 
+            // Ex2
+            // 
+            this.Ex2.Location = new System.Drawing.Point(465, 329);
+            this.Ex2.Name = "Ex2";
+            this.Ex2.Size = new System.Drawing.Size(53, 23);
+            this.Ex2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(364, 331);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Количество:";
+            // 
+            // Ex3
+            // 
+            this.Ex3.Location = new System.Drawing.Point(761, 329);
+            this.Ex3.Name = "Ex3";
+            this.Ex3.Size = new System.Drawing.Size(53, 23);
+            this.Ex3.TabIndex = 11;
+            this.Ex3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(660, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Количество:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(660, 394);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 25);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Очистить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(364, 394);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 25);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Очистить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(68, 394);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(154, 25);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Очистить";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(906, 424);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.Ex3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Ex2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Ex1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Ex1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ex2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ex3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown Ex1;
+        private System.Windows.Forms.NumericUpDown Ex2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown Ex3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vasilev5
+{
+
+  
+    enum Bank { текущий, сберегательный}
+
+    struct Bank_Info
+    {
+        public int number;
+        public Bank type;
+        public double balance;
+    }
+
+    enum University { КемГУ, НГАХА, МФТИ, ТУСУР }
+    struct Student
+    {
+        public string name;
+        public University university;
+    }
+
+    public partial class Form1 : Form
+    {
+        internal int iEx1 = 1;
+        internal int iEx2 = 1;
+        internal string[] names = new string[] {"София", "Михаил", "Дарья", "Юлия","Маргарита","Марк","Акулбек","Константин", "Валерий", "НикитаЛучший"};
+        internal Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Задание 1\n";
+            richTextBox2.Text = "Задание 2\n";
+            richTextBox3.Text = "Задание 3\n";
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i< Ex1.Value; i++)
+            {
+                Bank bank = new Bank();
+                bank = (Bank)rnd.Next(0, 2);
+                richTextBox1.Text += $"-------\n{iEx1}: {bank}\n";
+                iEx1++;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Задание 1\n";
+            iEx1 = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Ex2.Value; i++)
+            {
+                Bank_Info bankInfo = new Bank_Info();
+                bankInfo.number = iEx2;
+                iEx2++;
+
+                bankInfo.type = (Bank)rnd.Next(0, 2);
+
+                bankInfo.balance = (double) Math.Round(rnd.NextDouble()*100000, 2);
+                richTextBox2.Text += $"-------\nНомер счёта: {bankInfo.number}\nТип счёта: {bankInfo.type}\nБаланс: {bankInfo.balance} $\n";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "Задание 2\n";
+            iEx2 = 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Ex1.Value; i++)
+            {
+                Student student = new Student();
+                student.name = names[rnd.Next(0, names.Length)];
+                student.university = (University)rnd.Next(0, 4);
+
+                richTextBox3.Text += $"-------\nИмя: {student.name}\nУниверситет: {student.university}\n";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox3.Text = "Задание 3\n";
+        }
+    }
+}
